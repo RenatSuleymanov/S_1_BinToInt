b676cb4d01c357eb1360473db7046a5c1611910d
RenatSuleymanov <47538102+RenatSuleymanov@users.noreply.github.com>
2020-05-15 15:29:46 +0300

    Добавлена  обработка ввода

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,12 @@ import java.util.Scanner;
         String str = in.nextLine();
 
         char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length;i++) {
+            if (chars[i]!='0' & chars[i]!='1') {
+                System.out.println("Введенное значение не является двоичным");
+                System.exit(1);
+            }
+        }
         int result = 0;
         int mult = 1;
         for (int i = str.length()-1; i >= 0; i--) {
